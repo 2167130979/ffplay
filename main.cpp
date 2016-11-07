37532cfcecf49e4221e9b52db17003a808547497
Your Name <you@example.com>
2016-11-07 11:04:31 +0800

    消息循环

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include "ffplay.h"
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+void OnSize(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+void OnKeyUp(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+void OnClick(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+void OnTimer(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 BOOL  OpenFileDialog(HWND hWnd);
-char szClassName[] = "CodeBlocksWindowsApp";
 char szFile[2048];
 
-;
 int WINAPI WinMain(HINSTANCE hThisInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpszArgument,
@@ -17,7 +19,7 @@ int WINAPI WinMain(HINSTANCE hThisInstance,
 	WNDCLASSEX wincl;        /* Data structure for the windowclass */
 
 	wincl.hInstance = hThisInstance;
-	wincl.lpszClassName = szClassName;
+	wincl.lpszClassName = "ffplayapp";
 	wincl.lpfnWndProc = WindowProcedure;
 	wincl.style = CS_DBLCLKS;
 	wincl.cbSize = sizeof (WNDCLASSEX);
@@ -29,14 +31,12 @@ int WINAPI WinMain(HINSTANCE hThisInstance,
 	wincl.cbWndExtra = 0; 
 	wincl.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
 
-	/* Register the window class, and if it fails quit the program */
-	if (!RegisterClassEx(&wincl))
-		return 0;
+	RegisterClassEx(&wincl);
 
 	/* The class is registered, let's create the program*/
 	hwnd = CreateWindowEx(
 		0,                   /* Extended possibilites for variation */
-		szClassName,         /* Classname */
+		"ffplayapp",         /* Classname */
 		"ffplay",       /* Title Text */
 		WS_OVERLAPPEDWINDOW, /* default window */
 		CW_USEDEFAULT,       /* Windows decides the position */
@@ -49,107 +49,38 @@ int WINAPI WinMain(HINSTANCE hThisInstance,
 		NULL                 /* No Window Creation data */
 		);
 
-	/* Make the window visible on the screen */
 	ShowWindow(hwnd, nCmdShow);
-
-	/* Run the message loop. It will run until GetMessage() returns 0 */
+	SetTimer(hwnd, 1, 1000, NULL);
+	Init(hwnd);
 	while (GetMessage(&messages, NULL, 0, 0))
 	{
-		/* Translate virtual-key messages into character messages */
 		TranslateMessage(&messages);
-		/* Send message to WindowProcedure */
 		DispatchMessage(&messages);
 	}
 
-	/* The program return-value is 0 - The value that PostQuitMessage() gave */
 	return messages.wParam;
 }
 
-
-/*  This function is called by the Windows function DispatchMessage()  */
-
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	RECT rect;
-	double srcRatio,screenRatio;
-	double width, height;
-	double pos,incr;
-	char title[64];
-	LONGLONG time;
-	switch (message)                  /* handle the messages */
+{	
+	switch (message)
 	{
-	case WM_LBUTTONDOWN:	
-		SetTimer(hwnd, 1, 1000, NULL);
-		if (!is){
-			if (OpenFileDialog(hwnd) != 0){
-				Init(hwnd);
-				play(szFile);
-			}			
-		}
-		else
-			stream_toggle_pause(is);
+	case WM_LBUTTONDOWN:		
+		OnClick(hwnd, message, wParam, lParam);
 		break;
 	case WM_SIZE:
-		if (wParam != SIZE_MINIMIZED){
-			GetClientRect(hwnd, &rect);
-			srcRatio = (double)src_width / (double)src_height;
-			screenRatio = (double)(rect.right - rect.left) / (double)(rect.bottom - rect.top);
-			if (screenRatio > srcRatio){
-				height = rect.bottom - rect.top;
-				width = (rect.bottom - rect.top)*srcRatio;
-			}
-			else{
-				width = (rect.right - rect.left);
-				height = width / srcRatio;				
-			}
-			glViewport(((rect.right - rect.left) - width) / 2, ((rect.bottom - rect.top) - height) / 2, width, height);
-		}		
+		OnSize(hwnd,message,wParam,lParam);
 		break;
-	case WM_KEYUP:
-		if (!is)
-			break;
-		switch (wParam){
-		case VK_RIGHT:
-			incr = 60.0;
-			goto do_seek;
-			break;
-		case VK_LEFT:
-			incr = -60.0;
-			goto do_seek;
-			break;
-		case VK_DOWN:
-			incr = 600.0;
-			goto do_seek;
-			break;
-		case VK_UP:
-			incr = -600.0;
-			goto do_seek;
-			break;
-		do_seek:
-			pos = get_master_clock(is);
-			if (isnan(pos))
-				pos = (double)is->seek_pos / AV_TIME_BASE;
-			pos += incr;
-			if (is->ic->start_time != AV_NOPTS_VALUE && pos < is->ic->start_time / (double)AV_TIME_BASE)
-				pos = is->ic->start_time / (double)AV_TIME_BASE;
-			stream_seek(is, (int64_t)(pos * AV_TIME_BASE), (int64_t)(incr * AV_TIME_BASE), 0);
-		}
-	
+	case WM_KEYUP:		
+		OnKeyUp(hwnd, message, wParam, lParam);
 		break;
 	case WM_TIMER:
-		if (is)
-		{
-			memset(title, 0, 64);
-			time = get_master_clock(is);
-			sprintf(title, "%02lld:%02lld:%02lld/%02lld:%02lld:%02lld", time / 3600, time / 60 % 60, time % 60, totaltime / 3600, totaltime / 60 % 60, totaltime % 60);
-			SetWindowText(hwnd, title);
-		}
-		
+		OnTimer(hwnd, message, wParam, lParam);		
 		break;
 	case WM_DESTROY:
-		exit(0);       /* send a WM_QUIT to the message queue */
+		exit(0);
 		break;
-	default:                      /* for messages that we don't deal with */
+	default:
 		return DefWindowProc(hwnd, message, wParam, lParam);
 	}
 
@@ -164,5 +95,83 @@ BOOL  OpenFileDialog(HWND hWnd)
 	ofn.lpstrFile = szFile;
 	ofn.nMaxFile = sizeof(szFile);
 	ofn.lpstrFile[0] = '\0';
-	return(GetOpenFileName((LPOPENFILENAME)&ofn));
+	return GetOpenFileName((LPOPENFILENAME)&ofn);
+}
+
+void OnSize(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	RECT rect;
+	double srcRatio, screenRatio;
+	double width, height;
+	if (wParam != SIZE_MINIMIZED){
+		GetClientRect(hwnd, &rect);
+		srcRatio = (double)src_width / (double)src_height;
+		screenRatio = (double)(rect.right - rect.left) / (double)(rect.bottom - rect.top);
+		if (screenRatio > srcRatio){
+			height = rect.bottom - rect.top;
+			width = (rect.bottom - rect.top)*srcRatio;
+		}
+		else{
+			width = (rect.right - rect.left);
+			height = width / srcRatio;
+		}
+		glViewport(((rect.right - rect.left) - width) / 2, ((rect.bottom - rect.top) - height) / 2, width, height);
+	}
+}
+
+void OnKeyUp(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	double pos, incr;
+	if (!is)
+		return;
+	switch (wParam){
+	case VK_RIGHT:
+		incr = 60.0;
+		goto do_seek;
+		break;
+	case VK_LEFT:
+		incr = -60.0;
+		goto do_seek;
+		break;
+	case VK_DOWN:
+		incr = 600.0;
+		goto do_seek;
+		break;
+	case VK_UP:
+		incr = -600.0;
+		goto do_seek;
+		break;
+	do_seek:
+		pos = get_master_clock(is);
+		if (isnan(pos))
+			pos = (double)is->seek_pos / AV_TIME_BASE;
+		pos += incr;
+		if (is->ic->start_time != AV_NOPTS_VALUE && pos < is->ic->start_time / (double)AV_TIME_BASE)
+			pos = is->ic->start_time / (double)AV_TIME_BASE;
+		stream_seek(is, (int64_t)(pos * AV_TIME_BASE), (int64_t)(incr * AV_TIME_BASE), 0);
+	}
+}
+
+void OnClick(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	if (!is){
+		if (OpenFileDialog(hwnd) != 0){
+			play(szFile);
+		}
+	}
+	else
+		stream_toggle_pause(is);
+}
+
+void OnTimer(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	char title[64];
+	LONGLONG time;
+	if (is)
+	{
+		memset(title, 0, 64);
+		time = get_master_clock(is);
+		sprintf(title, "%02lld:%02lld:%02lld/%02lld:%02lld:%02lld", time / 3600, time / 60 % 60, time % 60, totaltime / 3600, totaltime / 60 % 60, totaltime % 60);
+		SetWindowText(hwnd, title);
+	}
 }
